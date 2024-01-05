31ce9b59199d37d5909443554903cc51fdf45466
michdr <michael.druk@mostly.ai>
2024-01-05 17:40:30 +0100

    wip

--- tools/model.py
+++ tools/model.py
@@ -1,8 +1,4 @@
 class Connector:
-    # skip
-    # Those comments
-    # will not be copied
-    # /skip
     def locations(self, prefix: str = "") -> list:
         if self.client and hasattr(self.client, "locations"):
             return self.client.locations(connector_id=self.id, prefix=prefix)
