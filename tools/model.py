3952e8d4455b4743a6f024cba16b145c477627f5
michdr <michael.druk@mostly.ai>
2024-01-10 11:38:16 +0100

    wip

--- tools/model.py
+++ tools/model.py
@@ -1,27 +1,113 @@
+from typing import Optional
+
+import pandas as pd
+
+
 class Connector:
     def locations(self, prefix: str = "") -> list:
+        """Retrieve the locations associated with this connector and prefix.
+
+        :param prefix: A prefix to filter the locations, defaults to "".
+        :return: A list of locations (schemas, databases, directories, etc.) on the
+            given level.
+        """
         if self.client and hasattr(self.client, "locations"):
             return self.client.locations(connector_id=self.id, prefix=prefix)
 
 
 class Generator:
-    def add_table(self, **kwargs) -> "SourceTable":
+    def add_table(
+        self,
+        sourceConnectorId: str,
+        location: str,
+        name: Optional[str] = None,
+        includeChildren: Optional[bool] = False,
+        modelConfiguration: Optional["ModelConfiguration"] = None,
+        textModelConfiguration: Optional["ModelConfiguration"] = None,
+    ) -> "SourceTable":
+        """Add table to generator.
+
+        :param sourceConnectorId: The unique identifier of a connector
+        :param location: The location of a source table
+        :param name: The name of a source table
+        :param includeChildren: If true, all tables that are referenced by foreign keys
+            will be included. If false, only the selected table will be included.
+        :param modelConfiguration:
+        :param textModelConfiguration:
+        :return: The added source table object
+        """
+        params = {
+            "sourceConnectorId": sourceConnectorId,
+            "location": location,
+            "name": name,
+            "includeChildren": includeChildren,
+            "modelConfiguration": modelConfiguration,
+            "textModelConfiguration": textModelConfiguration,
+        }
         if self.client and hasattr(self.client, "add_table"):
-            return self.client.add_table(generator_id=self.id, **kwargs)
+            return self.client.add_table(generator_id=self.id, **params)
 
-    def add_table_by_upload(self, **kwargs) -> "SourceTable":
+    def add_table_by_upload(
+        self,
+        file: str,
+        name: Optional[str] = None,
+        primaryKey: Optional[str] = None,
+        modelConfiguration: Optional["ModelConfiguration"] = None,
+        textModelConfiguration: Optional["ModelConfiguration"] = None,
+    ) -> "SourceTable":
+        """Add table to generator by uploading a data file.
+
+        :param file: Path to the file to upload
+        :param name: The name of a source table
+        :param includeChildren: If true, all tables that are referenced by foreign keys
+            will be included. If false, only the selected table will be included.
+        :param modelConfiguration:
+        :param textModelConfiguration:
+        :return: The added source table object
+        """
+        params = {
+            "file": file,
+            "name": name,
+            "primaryKey": primaryKey,
+            "modelConfiguration": modelConfiguration,
+            "textModelConfiguration": textModelConfiguration,
+        }
         if self.client and hasattr(self.client, "add_table_by_upload"):
-            return self.client.add_table_by_upload(generator_id=self.id, **kwargs)
+            return self.client.add_table_by_upload(generator_id=self.id, **params)
 
-    def add_table_from_df_by_upload(self, **kwargs) -> "SourceTable":
+    def add_table_from_df_by_upload(
+        self,
+        df: pd.DataFrame,
+        name: Optional[str] = None,
+        primaryKey: Optional[str] = None,
+        modelConfiguration: Optional["ModelConfiguration"] = None,
+        textModelConfiguration: Optional["ModelConfiguration"] = None,
+    ) -> "SourceTable":
+        """Add table to generator from a pd.DataFrame object.
+
+        :param df: DataFrame that represents the table to be added
+        :param name: The name of a source table
+        :param includeChildren: If true, all tables that are referenced by foreign keys
+            will be included. If false, only the selected table will be included.
+        :param modelConfiguration:
+        :param textModelConfiguration:
+        :return: The added source table object
+        """
         if self.client and hasattr(self.client, "add_table_from_df_by_upload"):
+            params = {
+                "df": df,
+                "name": name,
+                "primaryKey": primaryKey,
+                "modelConfiguration": modelConfiguration,
+                "textModelConfiguration": textModelConfiguration,
+            }
             return self.client.add_table_from_df_by_upload(
-                generator_id=self.id, **kwargs
+                generator_id=self.id, **params
             )
 
-    def get_table(self, **kwargs) -> "SourceTable":
+    def get_table(self, table_id: str) -> "SourceTable":
         if self.client and hasattr(self.client, "get_table"):
-            return self.client.get_table(generator_id=self.id, **kwargs)
+            return self.client.get_table(generator_id=self.id, table_id=table_id)
 
     def update_table(self, **kwargs) -> "SourceTable":
         if self.client and hasattr(self.client, "update_table"):
