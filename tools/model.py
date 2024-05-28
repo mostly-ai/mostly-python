acac1518e278ac1e60d298077a91b88fe7c7f0e4
Michael Platzer <michael.platzer@gmail.com>
2024-05-28 14:52:33 +0200

    SLFT

--- tools/model.py
+++ tools/model.py
@@ -205,7 +205,9 @@ class SyntheticDataset:
         :param file_path: The file path to save the synthetic dataset
         """
         bytes, filename = self.client._download(
-            synthetic_dataset_id=self.id, ds_format=format
+            synthetic_dataset_id=self.id,
+            ds_format=format,
+            short_lived_file_token=self.metadata.short_lived_file_token
         )
         file_path = Path(file_path or ".")
         if file_path.is_dir():
@@ -221,7 +223,7 @@ class SyntheticDataset:
 
         :return: The synthetic dataset as dictionary of pandas DataFrames
         """
-        dfs = self.client._data(synthetic_dataset_id=self.id)
+        dfs = self.client._data(synthetic_dataset_id=self.id, short_lived_file_token=self.metadata.short_lived_file_token)
         if return_type == "auto" and len(dfs) == 1:
             return list(dfs.values())[0]
         else:
