fb8e00754526e33ab91697b166d043130bb5dabe
Michael Platzer <michael.platzer@gmail.com>
2024-06-06 09:24:31 +0200

    return URL when calling .open()

--- mostlyai/base.py
+++ mostlyai/base.py
@@ -210,7 +210,7 @@ class CustomBaseModel(BaseModel):
             rich.print(self.dict())
         return capture.get()
 
-    def open(self):
+    def open(self) -> str:
         """
         Opens the instance in a web browser.
         """
@@ -218,6 +218,7 @@ class CustomBaseModel(BaseModel):
             raise APIError("Cannot open the instance")
         url = "/".join([self.client.base_url, *self.OPEN_URL_PARTS, str(self.id)])
         webbrowser.open_new(url)
+        return url
 
     def reload(self):
         """
