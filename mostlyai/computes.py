31d7b8952428b77b2ef21c23555e037b0a1c92f6
michdr <2467184+michdr@users.noreply.github.com>
2024-08-16 11:42:58 +0200

    implement computes api

---
+++ mostlyai/computes.py
@@ -0,0 +1,67 @@
+from typing import Any
+
+from mostlyai.base import DELETE, GET, POST, _MostlyBaseClient, PATCH
+from mostlyai.model import (
+    Compute,
+)
+
+
+class _MostlyComputesClient(_MostlyBaseClient):
+    SECTION = ["computes"]
+
+    # PUBLIC METHODS #
+
+    def list(self) -> list[Compute]:
+        response = self.request(verb=GET, path=[])
+        response = [Compute(**compute) for compute in response]
+        return response
+
+    def create(
+        self,
+        config: dict[str, Any],
+    ) -> Compute:
+        """
+        Create a compute. Only accessible for SuperAdmins.
+        The structures of the config and secrets parameters depend on the compute type.
+          ```yaml
+          - type: KUBERNETES
+            config:
+              toleration: string
+          - type: DATABRICKS
+            config:
+              instanceURL: string
+              clusterID: string
+            secrets:
+              token: string
+          ```
+        """
+        response = self.request(verb=POST, path=[], json=config, response_type=Compute)
+        return response
+
+    def get(self, compute_id: str) -> Compute:
+        """
+        Retrieve compute.
+
+        :param compute_id: The unique identifier of a compute
+        :return: The retrieved compute
+        """
+        response = self.request(verb=GET, path=[compute_id], response_type=Compute)
+        return response
+
+    # PRIVATE METHODS #
+
+    def _update(
+        self,
+        compute_id: str,
+        config: dict[str, Any],
+    ) -> Compute:
+        response = self.request(
+            verb=PATCH,
+            path=[compute_id],
+            json=config,
+            response_type=Compute,
+        )
+        return response
+
+    def _delete(self, compute_id: str) -> None:
+        self.request(verb=DELETE, path=[compute_id])
