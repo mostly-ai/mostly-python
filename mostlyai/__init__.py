9ada54702acef13f097e750a7714b78fcc5595a5
Michael Platzer <michael.platzer@gmail.com>
2024-12-19 17:59:22 +0100

    move back up

--- mostlyai/__init__.py
+++ mostlyai/__init__.py
@@ -12,3 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from mostlyai.client.api import MostlyAI
+
+__all__ = ["MostlyAI"]
+__version__ = "0.4.11"  # Do not set this manually. Use poetry version [params].
