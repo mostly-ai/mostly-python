693e15904fcd8d0e893beaa2e5454fa950acd142
mostlyai-ci <37445847+mostlyai-ci@users.noreply.github.com>
2024-12-19 22:00:09 +0400

    Bump version to 0.6.2 (#113)

--- mostlyai/__init__.py
+++ mostlyai/__init__.py
@@ -15,4 +15,4 @@
 from mostlyai.client.api import MostlyAI
 
 __all__ = ["MostlyAI"]
-__version__ = "0.4.11"  # Do not set this manually. Use poetry version [params].
+__version__ = "0.6.2"  # Do not set this manually. Use poetry version [params].
